500facc37dde3dc5fd9870933c62c2ca4f2c5813
Vikas Potluri <vikaspotluri123.github@gmail.com>
2019-12-03 10:29:08 -0600

    lint

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
-const compileThemes = require('./lib/compile-themes');
 const {writeFile} = require('fs').promises;
+const compileThemes = require('./lib/compile-themes');
 
 async function run() {
 	const themeConfig = await compileThemes();
@@ -8,5 +8,6 @@ async function run() {
 
 run().catch(error => {
 	console.log(error.excludeStack ? error.toString() : error);
+	// eslint-disable-next-line unicorn/no-process-exit
 	process.exit(1);
 });
