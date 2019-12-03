d82857d1db014fa0905f79e007a350e17dc80104
Vikas Potluri <vikaspotluri123.github@gmail.com>
2019-12-02 19:31:59 -0600

    Add build script for reading and storing themes

---
+++ build.js
@@ -0,0 +1,39 @@
+// @ts-check
+
+const {readdir} = require('fs').promises;
+const {resolve} = require('path');
+const Queue = require('./lib/queue');
+const parse = require('./lib/parsers/0.1');
+
+const THEMES_DIRECTORY = resolve(__dirname, 'themes');
+
+let fileList;
+const themes = {};
+const queue = new Queue(processThemeFile);
+queue.events.on('idle', terminate);
+
+async function processThemeFile(filePath) {
+	// convert `/full/path/to/theme.yaml` to `theme`
+	const key = filePath.split('.yaml').shift().split('/').pop().toLowerCase();
+	const payload = await parse(`themes/${filePath}`, key);
+
+	if (key in themes) {
+		throw new Error(`Duplicate theme for ${key}`);
+	}
+
+	delete payload.version;
+	themes[key] = payload;
+}
+
+function terminate() {
+	queue.dumpErrors();
+	console.log(`Finished processing ${fileList.length} theme${fileList.length === 1 ? '' : 's'}`);
+	process.exit(queue.numErrors);
+}
+
+async function run() {
+	fileList = (await readdir(THEMES_DIRECTORY)).filter(file => /\.yaml$/.test(file));
+	queue.addAll(fileList);
+}
+
+run().catch(console.log);
