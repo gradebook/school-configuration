56997f1600a9c8ed086f4681d927e10afc346db5
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-08-18 18:15:48 -0500

    Add post-deploy webhook

---
+++ scripts/reload.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+
+// @ts-check
+const {sendPayload} = require('@gradebook/actions-hook');
+
+async function doTheWork() {
+	try {
+		const REQUIRED_KEYS = ['GITHUB_REF', 'GITHUB_REPOSITORY', 'GITHUB_SHA'];
+
+		for (const key of REQUIRED_KEYS) {
+			if (!(key in process.env)) {
+				console.warn(`Missing key: ${key}. Not running post-deploy hook`);
+				process.exit(0);
+			}
+		}
+
+		console.log('Sending post-deploy notification to webhook');
+
+		await sendPayload({
+			payload: '{}',
+			onlyIf: {
+				isPush: true,
+				branch: 'master'
+			}
+		});
+	} catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
+}
+
+doTheWork();
