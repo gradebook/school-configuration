cc48a6dfa172b4aab809f67f6284e2aae344be45
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-12-20 20:20:07 -0600

    bump xo

--- lib/types/validation-array.ts
+++ lib/types/validation-array.ts
@@ -1,5 +1,7 @@
 export type ValidationArray = [string, (property: any) => boolean] | [string, (property: any) => boolean, boolean];
 
+// Records don't handle circular indexes well
+/* eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style */
 export type ValidationSchema = {
 	[property: string]: ValidationArray | ValidationSchema;
 };
