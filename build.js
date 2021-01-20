30061514e32e5c18883ece94a8dec8a10e4e7960
Ramsay Smith <ramsaysmith@outlook.com>
2021-01-20 00:12:31 -0600

    dynamically render webmanifest

--- build.js
+++ build.js
@@ -5,9 +5,10 @@ const {copy} = require('fs-extra');
 const compileSchools = require('./lib/compile-schools');
 const renderMaskableIcon = require('./lib/templates/icon-maskable');
 const renderFavicon = require('./lib/templates/favicon');
+const renderManifest = require('./lib/templates/webmanifest');
 
 async function run() {
-	await mkdir('./dist/api/v0', {recursive: true});
+	await mkdir('./dist/api/v1', {recursive: true});
 	const schoolList = await compileSchools();
 	const globalConfig = {};
 
@@ -18,12 +19,15 @@ async function run() {
 		const singleFile = {
 			[name]: contents
 		};
-		writeQueue.push(writeFile(`./dist/api/v0/${name}.json`, JSON.stringify(singleFile)));
-		writeQueue.push(writeFile(`./dist/api/v0/${name}-icon-maskable.svg`, renderMaskableIcon(contents.theme.primary)));
-		writeQueue.push(writeFile(`./dist/api/v0/${name}-favicon.svg`, renderFavicon(contents.theme.primary)));
+
+		await mkdir(`./dist/api/v1/${name}`, {recursive: true});
+		writeQueue.push(writeFile(`./dist/api/v1/${name}/config.json`, JSON.stringify(singleFile)));
+		writeQueue.push(writeFile(`./dist/api/v1/${name}/icon-maskable.svg`, renderMaskableIcon(contents.theme.primary)));
+		writeQueue.push(writeFile(`./dist/api/v1/${name}/favicon.svg`, renderFavicon(contents.theme.primary)));
+		writeQueue.push(writeFile(`./dist/api/v1/${name}/manifest.webmanifest`, renderManifest(name, contents.theme.primary)));
 	}
 
-	writeQueue.push(writeFile('./dist/api/v0/global.json', JSON.stringify(globalConfig)));
+	writeQueue.push(writeFile('./dist/api/v1/config.json', JSON.stringify(globalConfig)));
 
 	writeQueue.push(
 		copy('./site', './dist')
